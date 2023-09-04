4e3bc81a237a7e1719dc104e2c92b35e54ccb06b
kaminenisagar <kaminenisagar@gmail.com>
2023-09-04 05:26:41 +0000

    Solution

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <h1 className="heading"></h1>
+      </div>
+    )
+  }
+}
+export default LightDarkMode
